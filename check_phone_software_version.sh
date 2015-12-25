d5b2b37ba22aa9889654db40269c66aac3cb26c5
Sergei <shurubov@gmail.com>
2015-12-25 10:44:54 +0300

    Update check_phone_software_version.sh

--- check_phone_software_version.sh
+++ check_phone_software_version.sh
@@ -3,6 +3,9 @@ i=13000
 while [ "$i" -ne "13099" ]
 do
 /usr/sbin/asterisk -rx "sip show peer $i" | grep  Useragent |grep Yealin && /usr/sbin/asterisk -rx "sip show peer $i" | grep Addr && echo "number = $i"
+/usr/sbin/asterisk -rx "sip show peer $i" | grep  Useragent |grep Panas && /usr/sbin/asterisk -rx "sip show peer $i" | grep Addr && echo "number = $i"
+/usr/sbin/asterisk -rx "sip show peer $i" | grep  Useragent |grep Cisco && /usr/sbin/asterisk -rx "sip show peer $i" | grep Addr && echo "number = $i"
+/usr/sbin/asterisk -rx "sip show peer $i" | grep  Useragent |grep AddPac && /usr/sbin/asterisk -rx "sip show peer $i" | grep Addr && echo "number = $i"
 
 i=$(($i + 1))
 done
